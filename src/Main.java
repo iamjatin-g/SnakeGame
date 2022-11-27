221732813bfa439b61a92bba03c80c924de54405
iamjatin-g <jatinghind007@gmail.com>
2022-11-27 14:39:54 +0530

    Update.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
-// package snakegame;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-import javax.swing.Timer;
-import javax.swing.JFrame;
 
 public class Main extends MainGame {
 	public static void main(String[] args) {
